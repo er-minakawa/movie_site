3ea3ccf4867f8f060e1a735f1db2d1b3805ab8ec
minakawa <em24059@athome.co.jp>
2024-06-18 17:10:08 +0900

    20240618

--- user/user_page.php
+++ user/user_page.php
@@ -10,6 +10,7 @@ $pass = "testpass";
 if (isset($_POST) ==['reservation']) {
     // $user_id = $_POST["id"];
     $user_name = $_POST["name"];
+    $email = $_POST["email"];
     $movie = $_POST["movie"];
     $time = $_POST["time"];
 
@@ -19,14 +20,20 @@ if (isset($_POST) ==['reservation']) {
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // 予約情報をデータベースに挿入
-        $sql = "INSERT INTO user_data (name,movie,time) VALUES (?,?,?)";
+        $sql = "INSERT INTO user_data (name,email,movie,time) VALUES (?,?,?,?)";
         $stmt = $dbh->prepare($sql);
-        $stmt->execute([$user_name,$movie,$time]);
+        $stmt->execute([$user_name,$email,$movie,$time]);
 
         // 予約成功のメッセージを表示
-        echo "予約が完了しました。";
+        echo "予約が完了しました。確認メールを送信しました。";
+        echo '<p><a href="user.html">予約画面へ戻る</a></p>';
     } catch (PDOException $e) {
         die("PDO Error:" . $e->getMessage());
     }
+}
+if(isset($_POST['cancel']) && $_POST['cancel'] === 'キャンセル') {
+    // リダイレクトを実行する
+    header("Location:../top/index.html");
+    exit;
 }
 ?>
